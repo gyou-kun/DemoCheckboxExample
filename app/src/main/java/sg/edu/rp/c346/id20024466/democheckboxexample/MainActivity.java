1761fe4f967add13777faf29d674298515973e67
HAZIQ AQIL B MALIK NOENTIL <20024466@myrp.edu.sg>
2021-05-18 15:36:31 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20024466/democheckboxexample/MainActivity.java
@@ -0,0 +1,59 @@
+package sg.edu.rp.c346.id20024466.democheckboxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnabled;
+    Button btnCheck;
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cbEnabled = findViewById(R.id.checkBoxDiscount);
+        btnCheck = findViewById(R.id.buttonCheck);
+        tvShow = findViewById(R.id.textView);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyActivity", "Inside onClick()");
+                if(cbEnabled.isChecked()) {
+                    double pay = calcPay(100, 20);
+                    tvShow.setText("The discount is given. You need to pay " + pay);
+                }
+                else {
+                    double pay = calcPay(100, 0);
+                    tvShow.setText("The discount is not given. You need to pay " + pay);
+                }
+
+                Context context = getApplicationContext();
+                CharSequence buttonClickText = "Button Click";
+                int durationlong = Toast.LENGTH_LONG;
+                Toast buttonClick = Toast.makeText(context, buttonClickText, durationlong);
+                buttonClick.show();
+
+            }
+
+            private double calcPay(double price, double discount) {
+                double pay = price * (1 - discount/100);
+                return pay;
+            }
+        });
+
+
+
+    }
+}
